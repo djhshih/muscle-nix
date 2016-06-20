14d59b6a8b95a589407da3eee44011d36809a342
David J. H. Shih <djh.shih@gmail.com>
2016-06-20 15:51:31 -0400

    Initial commit.

---
+++ muscle.nix
@@ -0,0 +1,31 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  version = "3.8.1551";
+  name = "muscle-${version}";
+
+  src = fetchurl {
+    url = "http://www.drive5.com/muscle/muscle_src_${version}.tar.gz";
+    sha256 = "0bj8kj7sdizy3987zx6w7axihk40fk8rn76mpbqqjcnd64i5a367";
+  };
+
+	buildInputs = [ glibc ];
+
+  unpackPhase = ''
+    mkdir $name
+    tar -xzf $src -C $name
+    sourceRoot=$name
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin/
+    install muscle $out/bin/
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Multiple sequence alignment with high accuracy and high throughput";
+    license     = licenses.free;
+    homepage    = "http://www.drive5.com/muscle";
+    platforms   = platforms.unix;
+  };
+}
